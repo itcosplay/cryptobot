af192c570b199e0e9444a769edb01962b3a2f7fe
konstPialov <azure_dream@mail.ru>
2021-02-09 14:26:49 +0300

    was added admin  notifications

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ from aiogram import executor
 
 from loader import dp
 import handlers
-from utils.notify_admins import on_startup_notify
+from utils.notify_admins import on_startup_notify, on_shutdown_notify
 
 async def on_startup(dispatcher):
-    # Уведомляет про запуск
+    # Уведомляет про запуск бота
     await on_startup_notify(dispatcher)
+
+async def on_shutdown(dispatcher):
+    # Уведомляет про остановку бота
+    await on_shutdown_notify(dispatcher)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling (
+        dp, 
+        on_startup=on_startup, 
+        on_shutdown=on_shutdown
+    )
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -5,3 +5,6 @@ from data.config import ADMIN_ID
 
 async def on_startup_notify(dp: Dispatcher):
     await dp.bot.send_message(ADMIN_ID, 'Бот Запущен')
+
+async def on_shutdown_notify(dp: Dispatcher):
+    await dp.bot.send_message(ADMIN_ID, 'Бот Остановлен')
