52d7bebab689a39887d87152abb78718e772fbe6
konstPialov <azure_dream@mail.ru>
2021-03-10 21:26:32 +0300

    emo

--- handlers/users/in_processing/enter_to_processing.py
+++ handlers/users/in_processing/enter_to_processing.py
@@ -10,30 +10,14 @@ from states import Processing
 async def inter_to_processing(message:types.Message, state:FSMContext):
     await message.delete()
 
-    current_requests, in_processing, ready_to_give = sheet.get_numbs_processing_and_ready_requests()
+    current_requests, processing_req, ready_req = sheet.get_numbs_processing_and_ready_requests()
 
     await state.update_data(current_requests=current_requests)
     
-    if len(in_processing) == 0 and len(ready_to_give) == 0:
+    if len(processing_req) == 0 and len(ready_req) == 0:
         await message.answer('Все заявки исполненны.')
         
     else:
-
-        if len(in_processing) == 0:
-            await message.answer('Заявки со статусом "В обработке" отсутствуют')
-
-        else:
-
-            await message.answer (
-                'Заявки в обработке:',
-                reply_markup=create_kb_current_requests(in_processing))
-
-        if len(ready_to_give) == 0:
-            await message.answer('Заявки со статусом "Готово к выдаче" отсутствуют')
-
-        else:
-
-            await message.answer (
-                'Готовые к выдаче:',
-                reply_markup=create_kb_current_requests(ready_to_give)
-            )
+        await message.answer (
+            'Заявки в обработке:',
+            reply_markup=create_kb_current_requests(processing_req, ready_req))
--- keyboards/inline/in_processing/active_ready_requests.py
+++ keyboards/inline/in_processing/active_ready_requests.py
@@ -5,27 +5,48 @@ from emoji import emojize
 
 from keyboards.inline.callback_data import get_info_request_data
 
-## 'U+1F914'
 
-def create_kb_current_requests(list_of_numbs):
+def create_kb_current_requests(processing_req, ready_req):
     keyboard = InlineKeyboardMarkup()
-    smile = emojize(':thinking face:', use_aliases=True)
-    for request in list_of_numbs:
-        numb = request[0]
-        if request[1] != '-': rub = request[1] + 'RUB;'
-        else: rub = ''
-        if request[2] != '-': usd = request[2] + 'USD;'
-        else: usd = ''
-        if request[3] != '-': eur = request[3] + 'EUR;'
-        else: eur = ''
-        keyboard.add (
-            InlineKeyboardButton (
-                text = '{} #{};  {}  {}  {}'.format(smile, numb, rub, usd, eur),
-                callback_data = get_info_request_data.new (
-                    id=numb,
-                    type_btn='GETINFOREQUEST'
+    emo_process = emojize(':hourglass_flowing_sand:', use_aliases=True)
+    emo_ready = emojize(':money_with_wings:', use_aliases=True)
+
+    if len(processing_req) != 0:
+        for request in processing_req:
+            numb = request[0]
+            if request[1] != '-': rub = request[1] + 'RUB;'
+            else: rub = ''
+            if request[2] != '-': usd = request[2] + 'USD;'
+            else: usd = ''
+            if request[3] != '-': eur = request[3] + 'EUR;'
+            else: eur = ''
+            keyboard.add (
+                InlineKeyboardButton (
+                    text = '{}  #{}; {} {} {}'.format(emo_process, numb, rub, usd, eur),
+                    callback_data = get_info_request_data.new (
+                        id=numb,
+                        type_btn='GETINFOREQUEST'
+                    )
                 )
             )
-        )
+
+    if len(ready_req) != 0:
+        for request in ready_req:
+            numb = request[0]
+            if request[1] != '-': rub = request[1] + 'RUB;'
+            else: rub = ''
+            if request[2] != '-': usd = request[2] + 'USD;'
+            else: usd = ''
+            if request[3] != '-': eur = request[3] + 'EUR;'
+            else: eur = ''
+            keyboard.add (
+                InlineKeyboardButton (
+                    text = '{}  #{}; {} {} {}'.format(emo_ready, numb, rub, usd, eur),
+                    callback_data = get_info_request_data.new (
+                        id=numb,
+                        type_btn='GETINFOREQUEST'
+                    )
+                )
+            )
 
     return keyboard
