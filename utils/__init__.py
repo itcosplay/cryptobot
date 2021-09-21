21634d2493d8a3daa1c01eb880740e379734ccda
itcosplay <azure_dream@mail.ru>
2021-09-21 17:49:42 +0300

    updating log function is ready

--- utils/__init__.py
+++ utils/__init__.py
@@ -32,6 +32,7 @@ from .notify_universal import notify_someone_except_user
 from .get_beauty_sum import get_beauty_sum
 
 from .log_processor import get_request_as_string
+from .log_processor import updating_log
 
 from .googlesheets import send_to_google
 from .googlesheets import DataFromSheet
--- utils/log_processor.py
+++ utils/log_processor.py
@@ -1,5 +1,7 @@
 import copy
+import datetime
 import json
+
 
 
 def get_request_as_string(request):
@@ -10,3 +12,26 @@ def get_request_as_string(request):
     str_request = str_request.replace('"', r'\"')
 
     return str_request
+
+
+def updating_log (
+    update_type:str,
+    user:str,
+    request:str
+):
+    log_time = datetime.datetime.today().strftime("%H:%M %d/%m/%y")
+    updated_request = get_request_as_string(request)
+
+    log_data = {
+        'ACTION_NAME': update_type,
+        'action_date': log_time,
+        'user_name': user,
+        'entire_request': updated_request
+    }
+
+    full_log_data = request[9]
+    full_log_data = json.loads(full_log_data)
+    full_log_data.append(log_data)
+    full_log_data = json.dumps(full_log_data,  ensure_ascii=False)
+
+    return full_log_data
